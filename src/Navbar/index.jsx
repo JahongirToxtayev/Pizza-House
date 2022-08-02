98ed6f93d3fac166fd72f09c96473689c3a347f4
alideveloper <alideveloper09@gmail.com>
2022-08-02 03:19:18 -0500

    slider finish

--- src/Navbar/index.jsx
+++ src/Navbar/index.jsx
@@ -1,5 +1,34 @@
-import React, {useState} from 'react';
-import {Container, Wrapper, Button, LogoWrapper,Line,PizzasTitle, LogoWrapperBefore,BestAtmosPersonSubt,BestAtmosBtn,BestAtmosTitle,MenuIconDiv,BestAtmosDiv,ImgDiv,CartIcon,MenuSubtitle, CartItems,All,OurMenuTitle,MenuChild,MenuContainer,MenuParent, ImgWrapper, BgBlack} from "./style";
+import React, {useState, useEffect} from 'react';
+import {
+    Container,
+    Wrapper,
+    Button,
+    LogoWrapper,
+    Line,
+    PizzasTitle,
+    LogoWrapperBefore,
+    BestAtmosPersonSubt,
+    BestAtmosBtn,
+    BestAtmosTitle,
+    MenuIconDiv,
+    BestAtmosDiv,
+    ImgDiv,
+    CartIcon,
+    MenuSubtitle,
+    CartItems,
+    All,
+    OurMenuTitle,
+    MenuChild,
+    MenuContainer,
+    MenuParent,
+    ImgWrapper,
+    BgBlack,
+    SliderContainer,
+    Slide,
+    SlideBtn,
+    LeftArrow,
+    RightArrow
+} from "./style";
 import Logo from "../assets/images/logo-198x66.png"
 import Menu1 from "../assets/images/menu-1-370x278.jpg"
 import Menu2 from "../assets/images/menu-2-370x278.jpg"
@@ -7,40 +36,109 @@ import Menu3 from "../assets/images/menu-3-370x278.jpg"
 import Menu4 from "../assets/images/menu-4-370x278.jpg"
 import Menu5 from "../assets/images/menu-5-370x278.jpg"
 import Menu6 from "../assets/images/menu-6-370x278.jpg"
+import SliderImg1 from "../assets/images/slide-1-1920x753.jpeg"
+import SliderImg2 from "../assets/images/slide-2-1920x753.jpeg"
 import {TbLeaf} from "react-icons/tb";
 import {GiHamburger} from "react-icons/gi";
-import {IoFish,IoPizzaOutline} from "react-icons/io5";
+import {IoFish, IoPizzaOutline} from "react-icons/io5";
 import {FaIceCream} from "react-icons/fa";
 import {BiDrink} from "react-icons/bi";
 import {Link} from "react-router-dom";
+import {FiChevronLeft, FiChevronRight} from "react-icons/fi";
+import {ArrowLeft} from "@styled-icons/bootstrap/ArrowLeft"
+import {ArrowRight} from "@styled-icons/bootstrap/ArrowRight"
+import {AiOutlineArrowLeft} from "react-icons/ai"
+import {AiOutlineArrowRight} from "react-icons/ai"
+const data = [{id: 1, title: "PERFECT PIZZA", info: "Experience the taste of a perfect pizza at PizzaHouse, one of the best restaurants!"},
+    {id: 2, title: "NICE INGREDIENTS", info: "We use only the best ingredients to make one-of-a-kind pizzas for our customers."},
+    {id: 3,  title: "FAST DELIVERY", info: "Enjoy the fast delivery. Feel yourself to eat fresh meal like in onsite"}]
+
 
 function Index() {
     const [active, setActive] = useState("home")
     const [show, setShow] = useState(false)
+    const [slides, setSlides] = useState(data)
+    const [index, setIndex] = useState(0)
 
-    return (
-        <All>
+    const prevSlide = () => {
+        setIndex(oldIndex => {
+            let index = oldIndex - 1
+            if (index < 0) {
+                index = slides.length - 1
+            }
+            return index
+        })
+    }
+
+    useEffect(() => {
+        let slider = setInterval(() => {
+            setIndex(oldIndex => {
+                let index = oldIndex + 1
+                if (index > slides.length - 1) {
+                    index = 0
+                }
+                return index
+            })
+        }, 3000)
+        return () => {
+            clearInterval(slider)
+        }
+    }, [index])
+
+    const nextSlide = () => {
+        setIndex(oldIndex => {
+            let index = oldIndex + 1
+            if (index > slides.length - 1) {
+                index = 0
+            }
+            return index
+        })
+    }
+
+    return (<All>
             <Container>
                 <LogoWrapper>
                     <img style={{marginRight: "100px", maxHeight: "240px"}} src={Logo} alt=""/>
-                    <LogoWrapperBefore />
+                    <LogoWrapperBefore/>
                 </LogoWrapper>
                 <Wrapper>
                     <Link to="/">
                         <Button borderBottom={active === "home" && true} onClick={() => setActive("home")}>Home</Button>
                     </Link>
                     <Link to="/">
-                        <Button borderBottom={active === "about" && true} onClick={() => setActive("about")}>About us</Button>
+                        <Button borderBottom={active === "about" && true} onClick={() => setActive("about")}>About
+                            us</Button>
                     </Link>
                     <Link to="/">
-                        <Button borderBottom={active === "contacts" && true} onClick={() => setActive("contacts")}>Contact</Button>
+                        <Button borderBottom={active === "contacts" && true}
+                                onClick={() => setActive("contacts")}>Contact</Button>
                     </Link>
                 </Wrapper>
                 <Wrapper>
-                    <CartIcon />
+                    <CartIcon/>
                     {show && <CartItems>12</CartItems>}
                 </Wrapper>
             </Container>
+
+            <SliderContainer>
+                {slides.map((slide, slideIndex) => {
+                    const {id, title, info} = slide
+                    let position = "nextSlide"
+                    if (slideIndex === index) {
+                        position = "activeSlide"
+                    }
+                    if (slideIndex === index - 1 || (index === 0 && slideIndex === slides.length - 1)) {
+                        position = "lastSlide"
+                    }
+                    return <Slide  className={`slide ${position}`} key={id} bg={id}>
+                        <h1 style={{width: "50%", fontWeight: "400", fontSize: "130px", color: `${id === 3 ? "#FFE745" : "#fff"}`, margin: "30px 0 20px"}}>{title}</h1>
+                        <h3 style={{fontWeight: "400", fontSize: "20px", lineHeight: "1.55", color: "#fff", width: "40%"}}>{info}</h3>
+                        <SlideBtn style={{margin: "30px 0"}}>VIEW OUR SERVICES</SlideBtn>
+                    </Slide>
+                })}
+                <LeftArrow onClick={prevSlide}><AiOutlineArrowLeft style={{color: "#fff", fontWeight: "bold", fontSize: "30px", width: "30px"}} /></LeftArrow>
+                <RightArrow onClick={nextSlide}><AiOutlineArrowRight style={{color: "#fff", fontWeight: "bold", fontSize: "30px", width: "30px"}} /></RightArrow>
+            </SliderContainer>
 
             <div className='container my-5'>
                 <OurMenuTitle>OUR MENU</OurMenuTitle>
@@ -48,7 +146,7 @@ function Index() {
                     <MenuChild className="col-4">
                         <ImgDiv>
                             <ImgWrapper className="img-wrapper">
-                                <img className="img" src={Menu1} alt="menu-1" />
+                                <img className="img" src={Menu1} alt="menu-1"/>
                             </ImgWrapper>
                             <MenuIconDiv className="icon-wrapper"><TbLeaf className="icon"/>
                             </MenuIconDiv>
@@ -58,7 +156,7 @@ function Index() {
                     <MenuChild className="col-4">
                         <ImgDiv>
                             <ImgWrapper className="img-wrapper">
-                                <img className="img" src={Menu2} alt="menu-2" />
+                                <img className="img" src={Menu2} alt="menu-2"/>
                             </ImgWrapper>
                             <MenuIconDiv className="icon-wrapper"><IoPizzaOutline className="icon"/></MenuIconDiv>
                             <MenuSubtitle>Pizzas</MenuSubtitle>
@@ -67,7 +165,7 @@ function Index() {
                     <MenuChild className="col-4">
                         <ImgDiv>
                             <ImgWrapper className="img-wrapper">
-                                <img className="img" src={Menu3} alt="menu-3" />
+                                <img className="img" src={Menu3} alt="menu-3"/>
                             </ImgWrapper>
                             <MenuIconDiv className="icon-wrapper"><GiHamburger className="icon"/></MenuIconDiv>
                             <MenuSubtitle>Burgers</MenuSubtitle>
@@ -76,7 +174,7 @@ function Index() {
                     <MenuChild className="col-4">
                         <ImgDiv>
                             <ImgWrapper className="img-wrapper">
-                                <img className="img" src={Menu4} alt="menu-4" />
+                                <img className="img" src={Menu4} alt="menu-4"/>
                             </ImgWrapper>
                             <MenuIconDiv className="icon-wrapper"><FaIceCream className="icon"/></MenuIconDiv>
                             <MenuSubtitle>Desserts</MenuSubtitle>
@@ -85,16 +183,16 @@ function Index() {
                     <MenuChild className="col-4">
                         <ImgDiv>
                             <ImgWrapper className="img-wrapper">
-                                <img className="img" src={Menu5} alt="menu-5" />
+                                <img className="img" src={Menu5} alt="menu-5"/>
                             </ImgWrapper>
-                            <MenuIconDiv className="icon-wrapper"><BiDrink className="icon" /></MenuIconDiv>
+                            <MenuIconDiv className="icon-wrapper"><BiDrink className="icon"/></MenuIconDiv>
                             <MenuSubtitle>Drinks</MenuSubtitle>
                         </ImgDiv>
                     </MenuChild>
                     <MenuChild className="col-4">
                         <ImgDiv>
                             <ImgWrapper className="img-wrapper">
-                                <img className="img" src={Menu6} alt="menu-6" />
+                                <img className="img" src={Menu6} alt="menu-6"/>
                             </ImgWrapper>
                             <MenuIconDiv className="icon-wrapper"><IoFish className="icon"/></MenuIconDiv>
                             <MenuSubtitle>Seafood</MenuSubtitle>
@@ -102,22 +200,25 @@ function Index() {
                     </MenuChild>
                 </MenuParent>
             </div>
-                    
-                    <BestAtmosDiv className="row">
-                        <BgBlack />
-                        <div className="col-5" style={{zIndex: 1, display: "flex", flexDirection: "column", justifyContent: "center", textAlign: "left"}}>
-                            <BestAtmosTitle>Best atmosphere</BestAtmosTitle>
-                            <h3 style={{color: "#fff", fontSize: "24px"}}>PizzaHouse is the place of the best pizza and high-quality service.</h3>
-                            <div style={{display: "flex", alignItems: "center"}}><Line></Line><BestAtmosPersonSubt>Ben Smith, Founder</BestAtmosPersonSubt></div>
-                            <BestAtmosBtn>VIEW OUR SERVICES</BestAtmosBtn>
-                        </div>
-                    </BestAtmosDiv>
 
-                <div>
-                    <PizzasTitle>SELECTED PIZZAS</PizzasTitle>
+            <BestAtmosDiv className="row">
+                <BgBlack/>
+                <div className="col-5" style={{
+                    zIndex: 1, display: "flex", flexDirection: "column", justifyContent: "center", textAlign: "left"
+                }}>
+                    <BestAtmosTitle>Best atmosphere</BestAtmosTitle>
+                    <h3 style={{color: "#fff", fontSize: "24px"}}>PizzaHouse is the place of the best pizza and
+                        high-quality service.</h3>
+                    <div style={{display: "flex", alignItems: "center"}}><Line></Line><BestAtmosPersonSubt>Ben Smith,
+                        Founder</BestAtmosPersonSubt></div>
+                    <BestAtmosBtn>VIEW OUR SERVICES</BestAtmosBtn>
                 </div>
-        </All>
-    );
+            </BestAtmosDiv>
+
+            <div>
+                <PizzasTitle>SELECTED PIZZAS</PizzasTitle>
+            </div>
+        </All>);
 }
 
 export default Index;
